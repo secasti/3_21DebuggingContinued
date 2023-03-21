53d2c4355f1b7b1c27799eb23f1be01793c909d9
Selene Castillo <secast@live.com>
2023-03-21 12:48:12 -0700

    fixed Moira's code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gameoflife import width, height, stage, print_stage, count_neighbors
+import copy
 
 def init_stage(stage):
     for v_pos in range(0, height):
@@ -15,17 +16,20 @@ def init_stage(stage):
 # 4. Any dead cell with 3 neighbors comes alive
 
 def one_generation(stage):
-    for v_pos in range(len(stage)):
-        for h_pos in range(len(v_pos)):
-            neighbors = count_neighbors(stage, v_pos, h_pos)
-            if not stage[v_pos][h_pos] and neighbors == 3:
+    stage_copy = copy.deepcopy(stage)
+
+    for v_pos in range(len(stage_copy)):
+        for h_pos in range(len(stage_copy[v_pos])):
+            neighbors = count_neighbors(stage_copy, v_pos, h_pos)
+            if not stage_copy[v_pos][h_pos] and neighbors == 3:
                 stage[v_pos][h_pos] = True
-            elif stage[v_pos][h_pos] and neighbors < 2:
+            elif stage_copy[v_pos][h_pos] and neighbors < 2:
                 stage[v_pos][h_pos] = False
-            elif stage[v_pos][h_pos] and (neighbors == 2 or neighbors == 3):
+            elif stage_copy[v_pos][h_pos] and (neighbors == 2 or neighbors == 3):
                 stage[v_pos][h_pos] = True
-            elif stage[v_pos][h_pos] and neighbors > 3:
+            elif stage_copy[v_pos][h_pos] and neighbors > 3:
                 stage[v_pos][h_pos] = False
+
                 
 
 init_stage(stage)
